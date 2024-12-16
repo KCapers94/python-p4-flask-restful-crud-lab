5e953b61f3a9c9b3cf37928dcd1128b490a62a4e
Keith Capers <K.Capers94@gmail.com>
2024-12-16 14:02:04 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,32 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+
+        patch_plant = Plant.query.filter(Plant.id == id).first()
+        for attr in request.json:
+            setattr(patch_plant, attr, request.json[attr])
+
+        db.session.add(patch_plant)
+        db.session.commit()
+
+        response_dict = patch_plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+        
+        delete_plants = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(delete_plants)
+        db.session.commit()
+
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
